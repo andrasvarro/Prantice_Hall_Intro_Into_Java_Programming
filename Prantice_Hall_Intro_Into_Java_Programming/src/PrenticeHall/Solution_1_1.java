92464289335fc67fb93803104b2165607258c1a8
wp9om <wp9om@DESKTOP-4ND276E>
2017-01-02 13:56:18 +0100

    Add solution file for task 1.1

---
+++ Prantice_Hall_Intro_Into_Java_Programming/src/PrenticeHall/Solution_1_1.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PrenticeHall;
+
+/**
+ *
+ * @author wp9om
+ */
+public class Solution_1_1 {
+    
+}
