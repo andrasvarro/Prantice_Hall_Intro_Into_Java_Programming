7611dc164603bc099cc2d84d0924eaf6ba6b734d
andras.varro <>
2017-01-02 18:04:26 +0100

    Solution for task 1.1

--- Prantice_Hall_Intro_Into_Java_Programming/src/PrenticeHall/Solution_1_1.java
+++ Prantice_Hall_Intro_Into_Java_Programming/src/PrenticeHall/Solution_1_1.java
@@ -8,6 +8,26 @@ package PrenticeHall;
 /**
  * @author andras.varro
  */
+/* task:
+1.1 (Display three messages) Write a program that displays Welcome to Java,
+Welcome to Computer Science, and Programming is fun.
+*/
 public class Solution_1_1 {
-    
+    public static void main (String... args){
+        System.out.print("Welcome to Java, ");
+        System.out.print("Welcome to Computer Science, ");
+        System.out.print("Programming is fun");
+        // OR
+        // for a new line...
+        System.out.println();
+        System.out.print("Welcome to Java, Welcome to Computer Science, Programming is fun");
+        // OR
+        // for a new line...
+        System.out.println();        
+        // mulitlines
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+        System.out.print("Welcome to Java\nWelcome to Computer Science\nProgramming is fun\n");
+    }   
 }
