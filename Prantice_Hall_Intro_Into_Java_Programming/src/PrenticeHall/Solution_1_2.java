3e266789f16997d3571ded3eda06c27d020e5189
andras.varro <>
2017-01-03 20:10:21 +0100

    Solution for task 1.2

---
+++ Prantice_Hall_Intro_Into_Java_Programming/src/PrenticeHall/Solution_1_2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PrenticeHall;
+
+/**
+ *
+ * @author andras.varro
+ */
+public class Solution_1_2 {
+    public static void main (String[] args){
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        // OR
+        // adding empty line
+        System.out.println();
+        // with for
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Welcome to Java");
+        }
+    }
+}
