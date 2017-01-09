2b757457520af620568e2e8098f6221fe36187f3
andras.varro <>
2017-01-09 17:49:06 +0100

    Solution for task 1.5

---
+++ Prantice_Hall_Intro_Into_Java_Programming/src/PrenticeHall/Solution_1_5.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PrenticeHall;
+
+/**
+ *
+ * @author andras.varro
+ */
+/* Task:
+(Compute expressions) Write a program that displays the result of
+9.5 * 4.5 - 2.5 * 3
+-------------------
+    45.5 - 3.5
+.
+*/
+public class Solution_1_5 {
+    public static void main (String... args){
+        System.out.println((9.5 * 4.5 - 2.5 * 3)/(45.5 - 3.5));
+         
+    }
+}
